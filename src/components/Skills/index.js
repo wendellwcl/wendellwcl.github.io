0a5fac67e836fcea90b2bbdde03e561c031b7b7a
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-06-14 00:39:16 -0300

    configuração scrollreveal

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -11,7 +11,26 @@ import Styledcomponents from '../../assets/img/styledcomponents.png'
 import Github from '../../assets/img/github.png'
 import Git from '../../assets/img/git.png'
 
+import { useEffect } from 'react'
+import ScrollReveal from 'scrollreveal'
+
 const Skills = () => {
+
+    useEffect(() => {
+        ScrollReveal({reset: true})
+        ScrollReveal().reveal('.reveal1', {
+            delay: 100
+        })
+        ScrollReveal().reveal('.reveal2', {
+            delay: 200
+        })
+        ScrollReveal().reveal('.reveal3', {
+            delay: 300
+        })
+        ScrollReveal().reveal('.reveal4', {
+            delay: 400
+        })
+    }, [])
 
     return(
         <section id="skills-container">
@@ -19,25 +38,25 @@ const Skills = () => {
             <div className="container">
                 <div className="row row-cols-8 d-flex justify-content-around">
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal1">
                             <img src={Html} alt="icone html 5" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">html 5</h3>
                         </div>
                     </div>
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal2">
                             <img src={Css} alt="icone css 3" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">css 3</h3>
                         </div>
                     </div>
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal3">
                             <img src={Javascript} alt="icone javascript" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">javascript</h3>
                         </div>
                     </div>
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal4">
                             <img src={Bootstrap} alt="icone bootstrap" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">bootstrap</h3>
                         </div>
@@ -45,25 +64,25 @@ const Skills = () => {
                 </div>
                 <div className="row d-flex justify-content-around mt-0 mt-md-5">
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal1">
                             <img src={ReactIcon} alt="icone react" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">react</h3>
                         </div>
                     </div>
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal2">
                             <img src={Styledcomponents} alt="icone styled components" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">styled components</h3>
                         </div>
                     </div>
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div className="reveal3">
                             <img src={Github} alt="icone github" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">github</h3>
                         </div>
                     </div>
                     <div className="col-6 col-md-2">
-                        <div>
+                        <div  className="reveal4">
                             <img src={Git} alt="icone git" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">git</h3>
                         </div>
