cb006e9de4a5f2c2de5b4c280fe206929ab8c2fe
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2022-06-14 12:17:00 -0300

    ajustes na seção habilidades

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -34,55 +34,67 @@ const Skills = () => {
 
     return(
         <section id="skills-container">
-            <h2 className="text-center">Habilidades</h2>
+
+            <h2 className="text-center">Minhas Habilidades</h2>
+
             <div className="container">
+
                 <div className="row row-cols-8 d-flex justify-content-around">
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal1">
+                        <div className="reveal1 d-flex flex-column align-items-center">
                             <img src={Html} alt="icone html 5" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">html 5</h3>
                         </div>
                     </div>
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal2">
+                        <div className="reveal2 d-flex flex-column align-items-center">
                             <img src={Css} alt="icone css 3" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">css 3</h3>
                         </div>
                     </div>
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal3">
+                        <div className="reveal3 d-flex flex-column align-items-center">
                             <img src={Javascript} alt="icone javascript" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">javascript</h3>
                         </div>
                     </div>
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal4">
+                        <div className="reveal4 d-flex flex-column align-items-center">
                             <img src={Bootstrap} alt="icone bootstrap" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">bootstrap</h3>
                         </div>
                     </div>
                 </div>
+
                 <div className="row d-flex justify-content-around mt-0 mt-md-5">
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal1">
+                        <div className="reveal1 d-flex flex-column align-items-center">
                             <img src={ReactIcon} alt="icone react" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">react</h3>
                         </div>
                     </div>
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal2">
+                        <div className="reveal2 d-flex flex-column align-items-center">
                             <img src={Styledcomponents} alt="icone styled components" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">styled components</h3>
                         </div>
                     </div>
+
                     <div className="col-6 col-md-2">
-                        <div className="reveal3">
+                        <div className="reveal3 d-flex flex-column align-items-center">
                             <img src={Github} alt="icone github" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">github</h3>
                         </div>
                     </div>
+                    
                     <div className="col-6 col-md-2">
-                        <div  className="reveal4">
+                        <div  className="reveal4 d-flex flex-column align-items-center">
                             <img src={Git} alt="icone git" className="img-fluid glass"/>
                             <h3 className="text-center mb-5 mb-md-0">git</h3>
                         </div>
