d63e6b4ec711fddc123d239eb360d2a2728f5e9d
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-02-24 19:41:32 -0300

    feat: adding animations to Hero section elements and improving scss

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -2,6 +2,8 @@
 import { BsGithub, BsLinkedin } from "react-icons/bs";
 
 //Assets
+import arrowLeft from "../../assets/arrow_left.svg";
+import arrowRight from "../../assets/arrow_right.svg";
 import badgeImg from "../../assets/badge.png";
 
 //Styles
@@ -11,44 +13,59 @@ const HeroSection = () => {
     return (
         <section className={styles.hero_section}>
             <div className={styles.hero_container}>
-                <div className={styles.hero_greeting}>
-                    <h1 className={styles.greeting}>
-                        Olá,
-                        <br />
-                        eu sou <span className={styles.name}>Wendell</span>
-                    </h1>
-                    <h2 className={styles.role}>
-                        &lt; Desenvolvedor Front-End /&gt;
-                    </h2>
-                    <p className={styles.graduation}>
-                        3º semestre - Sistemas para internet
-                    </p>
+                <div className={styles.greeting_container}>
+                    <div className={styles.greeting_group}>
+                        <h1 className={styles.greeting}>
+                            Olá,
+                            <br />
+                            eu sou <span className={styles.name}>Wendell</span>
+                        </h1>
 
-                    <div className={styles.professional_networks}>
-                        <a
-                            className={styles.btn}
-                            href="https://www.linkedin.com/in/wendellwcl/"
-                            target="_blank"
-                        >
-                            <BsLinkedin />
-                            Linkedin
-                        </a>
-                        <a
-                            className={styles.btn}
-                            href="https://github.com/wendellwcl"
-                            target="_blank"
-                        >
-                            <BsGithub />
-                            Github
-                        </a>
+                        <h2 className={styles.role}>
+                            &lt; Desenvolvedor Front-End /&gt;
+                        </h2>
+
+                        <p className={styles.graduation}>
+                            3º semestre - Sistemas para internet
+                        </p>
+
+                        <div className={styles.professional_networks}>
+                            <a
+                                className={styles.btn}
+                                href="https://www.linkedin.com/in/wendellwcl/"
+                                target="_blank"
+                            >
+                                <BsLinkedin />
+                                Linkedin
+                            </a>
+                            <a
+                                className={styles.btn}
+                                href="https://github.com/wendellwcl"
+                                target="_blank"
+                            >
+                                <BsGithub />
+                                Github
+                            </a>
+                        </div>
                     </div>
                 </div>
 
-                <div className={styles.hero_img}>
-                    <img
-                        src={badgeImg}
-                        alt="imagem de um crachá com a foto do desenvolvedor do site"
-                    />
+                <div className={styles.img_container}>
+                    <div className={styles.badge_img_group}>
+                        <img
+                            className={styles.badge_img}
+                            src={badgeImg}
+                            alt="imagem de um crachá com a foto do desenvolvedor do site"
+                        />
+                        <img
+                            className={`${styles.arrow_img} left`}
+                            src={arrowLeft}
+                        />
+                        <img
+                            className={`${styles.arrow_img} right`}
+                            src={arrowRight}
+                        />
+                    </div>
                 </div>
             </div>
         </section>
