9aa7e4a96ab4a5251221f56d94acb208e2d238e8
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-03-08 17:59:45 -0300

    feat:  creating a form in the Contact section

--- src/sections/ContactSection/ContactSection.tsx
+++ src/sections/ContactSection/ContactSection.tsx
@@ -1,3 +1,5 @@
+import { useLayoutEffect, useRef } from "react";
+
 //Icons
 import {
     BsEnvelopeFill,
@@ -6,13 +8,12 @@ import {
     BsWhatsapp,
 } from "react-icons/bs";
 
-//Assets
-import phone from "../../assets/phone.png";
-
 //Styles
 import styles from "./ContactSection.module.css";
 
 const ContactSection = () => {
+    const submitBtnRef = useRef<HTMLButtonElement>(null);
+
     const contacts = [
         {
             element: <BsWhatsapp />,
@@ -35,6 +36,28 @@ const ContactSection = () => {
             text: "github.com/wendellwcl",
         },
     ];
+
+    //Logic for controlling input field animations
+    useLayoutEffect(() => {
+        document.querySelectorAll(".input_field").forEach((element) => {
+            element.addEventListener("blur", (event) => {
+                const target = event.target as HTMLInputElement;
+
+                if (target.value != "") {
+                    target.nextElementSibling?.classList.add("filled");
+                } else {
+                    target.nextElementSibling?.classList.remove("filled");
+                }
+            });
+        });
+    }, []);
+
+    //Display feedback on form submission
+    function handleSubmit() {
+        submitBtnRef.current!.disabled = true;
+        (submitBtnRef.current as HTMLElement).innerText =
+            "Por favor, aguarde...";
+    }
 
     return (
         <section className={styles.contact_section}>
@@ -65,8 +88,64 @@ const ContactSection = () => {
                             </div>
                         </div>
                     </div>
-                    <div className={styles.contact_img}>
-                        <img src={phone} alt="imagem de um telefone" />
+                    <div className={styles.form_container}>
+                        <form
+                            onSubmit={() => handleSubmit()}
+                            action="https://formsubmit.co/wendell.wcl19@gmail.com"
+                            method="POST"
+                        >
+                            <div className={styles.input_container}>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    className={`${styles.input_field} input_field`}
+                                    placeholder="Nome"
+                                    required
+                                />
+                                <label htmlFor="name" className={styles.label}>
+                                    Nome
+                                </label>
+                            </div>
+
+                            <div className={styles.input_container}>
+                                <input
+                                    type="email"
+                                    name="email"
+                                    id="email"
+                                    className={`${styles.input_field} input_field`}
+                                    placeholder="E-mail"
+                                    required
+                                />
+                                <label htmlFor="email" className={styles.label}>
+                                    E-mail
+                                </label>
+                            </div>
+
+                            <div className={styles.input_container}>
+                                <textarea
+                                    name="message"
+                                    id="message"
+                                    className={`${styles.input_field} input_field`}
+                                    placeholder="Menssagem"
+                                    required
+                                ></textarea>
+                                <label
+                                    htmlFor="message"
+                                    className={styles.label}
+                                >
+                                    Mensagem
+                                </label>
+                            </div>
+
+                            <button
+                                type="submit"
+                                className={styles.form_btn}
+                                ref={submitBtnRef}
+                            >
+                                Enviar
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
