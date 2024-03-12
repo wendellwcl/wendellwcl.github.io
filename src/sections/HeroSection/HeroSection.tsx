22660d9cdb17a877eb392a41d097e9becdba48a3
Wendell Corrêa Lima <wendell.wcl19@gmail.com>
2024-03-12 12:35:22 -0300

    refactor: review and organization of HeroSection files

--- src/sections/HeroSection/HeroSection.tsx
+++ src/sections/HeroSection/HeroSection.tsx
@@ -11,8 +11,8 @@ import styles from "./HeroSection.module.css";
 
 const HeroSection = () => {
     return (
-        <section className={styles.hero_section} id="hero-section">
-            <div className={styles.hero_container}>
+        <section className={styles.hero_section}>
+            <div className={`${styles.hero_container} scroll_animation`}>
                 <div className={styles.greeting_container}>
                     <div className={styles.greeting_wrapper}>
                         <h1 className={styles.greeting}>
